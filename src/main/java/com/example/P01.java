b282740f4529350502b6250572bd00481b49dcaa
Manish Dait <daitmanish88@gmail.com>
2024-11-17 20:36:21 +0530

    rename App.java

--- src/main/java/com/example/P01.java
+++ src/main/java/com/example/P01.java
@@ -6,7 +6,7 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 
-public class App {
+public class P01 {
   public static void main(String[] args) {
     EntityManagerFactory emf = Persistence.createEntityManagerFactory("pu");
     EntityManager em = emf.createEntityManager();
